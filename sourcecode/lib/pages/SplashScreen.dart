91d091866477d73d712f8950fc870223b7302e99
Calvin Reibenspieß <c.reibenspiess@lingner.com>
2023-11-11 09:51:44 +0100

    implement SplashScreen, add assets

---
+++ sourcecode/lib/pages/SplashScreen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile_computing_payment_app/pages/MainScreen.dart';
+
+var logo = SvgPicture.asset('assets/images/payero-logo-vertical-with-title.svg',
+    semanticsLabel: 'Payero App');
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(Duration(milliseconds: 1500), () {
+      Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                const MainScreen(),
+            transitionDuration: const Duration(milliseconds: 300),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              const begin = Offset(0.0, 1.0);
+              const end = Offset.zero;
+              final tween = Tween(begin: begin, end: end);
+              final offsetAnimation = animation.drive(tween);
+
+              return SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+            },
+          ));
+    });
+
+    return Scaffold(body: Center(child: logo));
+  }
+}
