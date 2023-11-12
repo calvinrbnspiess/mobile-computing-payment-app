d5bedeac579d76b75cb50849d72371355758e5ea
Calvin Reibenspieß <c.reibenspiess@lingner.com>
2023-11-12 17:44:06 +0100

    implement action button, improve layout to be more adaptive

--- sourcecode/lib/pages/MainScreen.dart
+++ sourcecode/lib/pages/MainScreen.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class HeaderRow extends StatelessWidget {
+  const HeaderRow({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: Stack(
         alignment: Alignment.center,
@@ -14,7 +16,7 @@ class HeaderRow extends StatelessWidget {
           Positioned(
             right: 0,
             child: IconButton(
-              icon: Icon(Icons.settings, size: 30),
+              icon: const Icon(Icons.settings, size: 30),
               onPressed: () {
                 // Handle cog wheel icon press
               },
@@ -27,25 +29,27 @@ class HeaderRow extends StatelessWidget {
 }
 
 class QrCode extends StatelessWidget {
+  const QrCode({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Image.asset('assets/images/sample-qr-code.png', width: 200),
-        padding: EdgeInsets.all(14),
-        margin: EdgeInsets.only(top: 70, bottom: 60),
+        padding: const EdgeInsets.all(14),
         decoration: BoxDecoration(
             color: Colors.white,
-            border: Border.all(color: Color(0xFFD6D6D6), width: 1),
-            borderRadius: BorderRadius.all(Radius.circular(16))));
+            border: Border.all(color: const Color(0xFFD6D6D6), width: 1),
+            borderRadius: const BorderRadius.all(Radius.circular(16))),
+        child: Image.asset('assets/images/sample-qr-code.png', width: 200));
   }
 }
 
 class TransactionEntry extends StatelessWidget {
-  String date = "";
-  String name = "";
-  double amount = 0.0;
+  final String date;
+  final String name;
+  final double amount;
 
-  TransactionEntry(this.date, this.name, this.amount, {super.key});
+  const TransactionEntry(
+      {super.key, this.date = "", this.name = "", this.amount = 0.0});
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +60,7 @@ class TransactionEntry extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(date, style: const TextStyle(color: const Color(0xff5F6060))),
+        Text(date, style: const TextStyle(color: Color(0xff5F6060))),
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Text(name, style: const TextStyle(fontSize: 16)),
           Text(formattedAmount,
@@ -73,26 +77,85 @@ class TransactionEntry extends StatelessWidget {
 }
 
 class TransactionHistory extends StatelessWidget {
+  const TransactionHistory({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Wrap(children: [
+      padding: const EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: const Color(0xFFD6D6D6), width: 1),
+          borderRadius: const BorderRadius.all(Radius.circular(16))),
+      child: const Wrap(runSpacing: 6, children: [
         Text(
           "Transaktionen",
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 10),
-        TransactionEntry("09.11.2023 13:34 Uhr", "Max Mustermann", -55.00),
-        TransactionEntry("26.10.2023 17:21 Uhr", "Tim Müller", 24.12),
-        TransactionEntry("13.07.2023 09:12 Uhr", "Felix Schmidt", 13.77)
-      ], runSpacing: 6),
-      padding: EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Color(0xFFD6D6D6), width: 1),
-          borderRadius: BorderRadius.all(Radius.circular(16))),
-      margin: EdgeInsets.only(left: 30, right: 30),
-      transform: Matrix4.translationValues(0.0, -75.0, 0.0),
+        TransactionEntry(
+            date: "09.11.2023 13:34 Uhr",
+            name: "Max Mustermann",
+            amount: -55.00),
+        TransactionEntry(
+            date: "26.10.2023 17:21 Uhr", name: "Tim Müller", amount: 24.12),
+        TransactionEntry(
+            date: "13.07.2023 09:12 Uhr", name: "Felix Schmidt", amount: 13.77)
+      ]),
+    );
+  }
+}
+
+class Pane extends StatelessWidget {
+  final List<Widget> children;
+  final double bottomOverlap;
+
+  const Pane({super.key, this.bottomOverlap = 0, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        padding: EdgeInsets.only(
+            bottom: 30 + bottomOverlap, top: 60, left: 30, right: 30),
+        decoration: BoxDecoration(
+            color: const Color(0xFFF3F5F7),
+            border: Border.all(color: const Color(0xFFD6D6D6), width: 1),
+            borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(24),
+                bottomRight: Radius.circular(24))),
+        child: Column(
+          children: children,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ));
+  }
+}
+
+class PayeroButton extends StatelessWidget {
+  const PayeroButton({this.text = "", super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print("Click event on Container");
+      },
+      child: Ink(
+        height: 66,
+        decoration: BoxDecoration(
+          color: const Color(0xff0FDFAF),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Center(
+          child: Text(text,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color(0xff393939))),
+        ),
+      ),
     );
   }
 }
@@ -102,20 +165,27 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var pane = Container(
-        width: double.infinity,
-        child: Column(
-            children: [HeaderRow(), QrCode()], mainAxisSize: MainAxisSize.min),
-        padding: EdgeInsets.only(bottom: 60, top: 60, left: 30, right: 30),
-        decoration: BoxDecoration(
-            color: Color(0xFFF3F5F7),
-            border: Border.all(color: Color(0xFFD6D6D6), width: 1),
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(24),
-                bottomRight: Radius.circular(24))));
+    const double overlap = 75;
 
     return Scaffold(
-        body: Container(child: Column(children: [pane, TransactionHistory()])),
-        backgroundColor: Colors.white);
+      body: Column(children: [
+        const Expanded(
+            child: Pane(
+          bottomOverlap: overlap,
+          children: [HeaderRow(), QrCode()],
+        )),
+        Container(
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            child: Column(children: [
+              Container(
+                transform: Matrix4.translationValues(0.0, overlap * -1.0, 0.0),
+                child: const Column(children: [
+                  TransactionHistory(),
+                ]),
+              ),
+              const PayeroButton(text: "Zum Bezahlen Scannen"),
+            ]))
+      ]),
+    );
   }
 }
