f4c26431915487f91fc04752cbf1896b3e3f7c63
florianbraendle <59693160+florianbraendle@users.noreply.github.com>
2023-11-19 22:43:00 +0100

    add new screen showing content of qr code

--- sourcecode/lib/pages/QRScanScreen.dart
+++ sourcecode/lib/pages/QRScanScreen.dart
@@ -2,71 +2,137 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class QRScanScreen extends StatelessWidget {
-  // const QRScanScreen({super.key});
+class QRScanScreen extends StatefulWidget {
+
+  const QRScanScreen({super.key});
+
+  @override
+  State<QRScanScreen> createState() => _QRScanScreenState();
+}
+
+class _QRScanScreenState extends State<QRScanScreen> {
   MobileScannerController cameraController = MobileScannerController();
-  // detectionSpeed = DetectionSpeed.noDuplicates
+  bool _screenOpened = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: SvgPicture.asset('assets/images/payero-logo.svg',
+      appBar: AppBar(
+        title: SvgPicture.asset(
+          'assets/images/payero-logo.svg',
           fit: BoxFit.scaleDown,
           alignment: Alignment.center,
           height: 50.0,
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFF3F5F7),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
           ),
-          centerTitle: true,
-          backgroundColor: const Color(0xFFF3F5F7),
-          actions: [
-            IconButton(
-              color: Colors.white,
-              icon: ValueListenableBuilder(
-                valueListenable: cameraController.torchState,
-                builder: (context, state, child) {
-                  switch (state as TorchState) {
-                    case TorchState.off:
-                      return const Icon(Icons.flash_off, color: Colors.grey);
-                    case TorchState.on:
-                      return const Icon(Icons.flash_on, color: Colors.yellow);
-                  }
-                },
-              ),
-              iconSize: 32.0,
-              onPressed: () => cameraController.toggleTorch(),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front, color: Colors.grey);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear, color: Colors.grey);
+                }
+              },
             ),
-            IconButton(
-              color: Colors.white,
-              icon: ValueListenableBuilder(
-                valueListenable: cameraController.cameraFacingState,
-                builder: (context, state, child) {
-                  switch (state as CameraFacing) {
-                    case CameraFacing.front:
-                      return const Icon(Icons.camera_front, color: Colors.grey);
-                    case CameraFacing.back:
-                      return const Icon(Icons.camera_rear, color: Colors.grey);
-                  }
-                },
+            iconSize: 32.0,
+            onPressed: () => cameraController.switchCamera(),
+          ),
+        ],
+      ),
+      body: MobileScanner(
+        startDelay: true,
+        controller: cameraController,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          if (barcodes.isNotEmpty && !_screenOpened) {
+            final String code = barcodes.first.rawValue ?? "---";
+            debugPrint('QRCode found! $code');
+            _screenOpened = true;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FoundCodeScreen(screenClosed: _screenWasClosed, value: code),
               ),
-              iconSize: 32.0,
-              onPressed: () => cameraController.switchCamera(),
-            ),
-          ],
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  void _screenWasClosed() {
+    _screenOpened = false;
+  }
+}
+
+class FoundCodeScreen extends StatefulWidget {
+  final String value;
+  final Function() screenClosed;
+  const FoundCodeScreen({
+    Key? key,
+    required this.value,
+    required this.screenClosed,
+  }) : super(key: key);
+
+  @override
+  State<FoundCodeScreen> createState() => _FoundCodeScreenState();
+}
+
+class _FoundCodeScreenState extends State<FoundCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: SvgPicture.asset('assets/images/payero-logo.svg',
+        fit: BoxFit.scaleDown,
+        alignment: Alignment.center,
+        height: 50.0,
         ),
-        body: MobileScanner(
-          startDelay: true,
-          // fit: BoxFit.contain,
-          // detectionSpeed: DetectionSpeed.noDuplicates,
-          controller: cameraController,
-          onDetect: (capture) {
-            final List<Barcode> barcodes = capture.barcodes;
-            // final Uint8List? image = capture.image;
-            for (final barcode in barcodes) {
-              debugPrint('Barcode found! ${barcode.rawValue}');
-            }
+        backgroundColor: const Color(0xFFF3F5F7),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            widget.screenClosed();
+            Navigator.pop(context);
           },
-          // onDetect: foundQRCode,
+          icon: const Icon(Icons.arrow_back_outlined,),
         ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Scanned Code:", style: TextStyle(fontSize: 20,),),
+              SizedBox(height: 20,),
+              Text(widget.value, style: TextStyle(fontSize: 16,),),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
