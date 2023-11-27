50b577a391605f3dbcb52085449ae0da9d591795
Calvin Reibenspieß <c.reibenspiess@lingner.com>
2023-11-27 17:01:26 +0100

    implement user registration, generate dynamic qr code

--- backend/src/app/register/route.ts
+++ backend/src/app/register/route.ts
@@ -1,3 +1,31 @@
-export async function GET() {
-  return Response.json({ hello: "world" });
+import { PrismaClient } from "@prisma/client";
+
+export async function POST(request: Request) {
+  const { nickname, email } = await request.json();
+
+  try {
+    let prisma = new PrismaClient();
+
+    let user = await prisma.user.create({
+      data: {
+        nickname: nickname,
+        email: email,
+      },
+    });
+
+    return Response.json({
+      success: true,
+      id: user.id,
+      username: user.nickname,
+      email: user.email,
+    });
+  } catch (error) {
+    console.log("Could not create user: ", error);
+    return Response.json(
+      { success: false, error: "Could not create user" },
+      {
+        status: 500,
+      }
+    );
+  }
 }
