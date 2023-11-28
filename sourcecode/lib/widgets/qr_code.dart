845f103b9f797f3621dee0e8494774a64450d994
Calvin Reibenspieß <c.reibenspiess@lingner.com>
2023-11-29 00:54:12 +0100

    add download button to qr code

--- sourcecode/lib/widgets/qr_code.dart
+++ sourcecode/lib/widgets/qr_code.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:mobile_computing_payment_app/widgets/payero_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class QrCode extends StatelessWidget {
@@ -10,16 +12,44 @@ class QrCode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Flexible(
-        child: Container(
-            constraints: BoxConstraints(minHeight: 0, maxHeight: 200),
-            padding: const EdgeInsets.all(4),
-            margin: const EdgeInsets.only(top: 18, bottom: 18),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: const Color(0xFFD6D6D6), width: 1),
-                borderRadius: const BorderRadius.all(Radius.circular(16))),
-            child: url != null
-                ? Image.network(url!)
-                : new CircularProgressIndicator()));
+        child: Stack(alignment: Alignment.center, children: [
+      Container(
+          constraints: const BoxConstraints(minHeight: 0, maxHeight: 200),
+          padding: const EdgeInsets.all(4),
+          margin: const EdgeInsets.only(top: 18, bottom: 26),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: const Color(0xFFD6D6D6), width: 1),
+              borderRadius: const BorderRadius.all(Radius.circular(16))),
+          child: url != null
+              ? Image.network(url!)
+              : new CircularProgressIndicator()),
+      Positioned(
+          bottom: 0,
+          child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(0xff0FDFAF)),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ))),
+              onPressed: () {
+                if (url == null) return;
+
+                debugPrint("Downloading image from $url");
+
+                ImageDownloader.downloadImage(url!).then((value) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("QR-Code wurde gespeichert.")));
+                }).catchError((error) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Fehler beim Herunterladen.")));
+                });
+              },
+              child: Text("Download")))
+    ]));
   }
 }
